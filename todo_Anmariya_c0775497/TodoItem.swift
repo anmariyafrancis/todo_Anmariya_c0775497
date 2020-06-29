ad829dc0d9c4f7d0c5bfce3e59ab6b775baf631a
anmariyafrancis <anmariya1915@gmail.com>
2020-06-29 15:37:11 -0400

    [AF] imported coredata

---
+++ todo_Anmariya_c0775497/TodoItem.swift
@@ -0,0 +1,14 @@
+//
+//  TodoItem.swift
+//  todo_Anmariya_c0775497
+//
+//  Created by user176498 on 6/29/20.
+//  Copyright © 2020 user176498. All rights reserved.
+//
+
+import Foundation
+
+class TodoItem {
+    var todoTitle: String!
+    
+}
