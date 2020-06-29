adf61293e5a939f3821f3d984d2838fac82952d8
anmariyafrancis <anmariya1915@gmail.com>
2020-06-29 15:27:38 -0400

    [AF] created tableviewcell

---
+++ todo_Anmariya_c0775497/pCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  pCellTableViewCell.swift
+//  todo_Anmariya_c0775497
+//
+//  Created by user176498 on 6/29/20.
+//  Copyright © 2020 user176498. All rights reserved.
+//
+
+import UIKit
+
+class pCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
