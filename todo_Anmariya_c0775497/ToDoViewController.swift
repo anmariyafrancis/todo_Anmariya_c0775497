42352c6cba1c26953e849c9f41a77ac7cccc591a
anmariyafrancis <anmariya1915@gmail.com>
2020-06-29 18:00:34 -0400

    [AF] NEW file added

--- todo_Anmariya_c0775497/ToDoViewController.swift
+++ todo_Anmariya_c0775497/ToDoViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ToDoViewController.swift
+//  TodoViewController.swift
 //  todo_Anmariya_c0775497
 //
 //  Created by user176498 on 6/29/20.
@@ -7,92 +7,82 @@
 //
 
 import UIKit
+
 import CoreData
 
 class TodoViewController: UIViewController {
 
+    
     @IBOutlet weak var todoTitleLabel: UITextField!
+    
+    var todo: Todo?
+//    delegate for previous screen to call methods
+    var delegate: TaskListViewController?
+    
+    @IBOutlet weak var deadlineLabel: UIDatePicker!
+    
+    @IBOutlet weak var buttonStack: UIStackView!
+    override func viewDidLoad() {
         
-         var todo: Todo?
-    //    delegate for previous screen to call methods
-        var delegate: TaskListViewController?
-        
-        @IBOutlet weak var deadlineLabel: UIDatePicker!
-        
-        @IBOutlet weak var buttonStack: UIStackView!
-        override func viewDidLoad() {
-            
-            super.viewDidLoad()
-    //        hides completed and deleted buttons if new todo
-            if todo == nil {
-                buttonStack.isHidden = true
-            }
-    //        sets the field values if old todo opened
-            if let todoData = todo
-            {
-                todoTitleLabel.text = todoData.name
-                deadlineLabel.date = todoData.due_date!
-            }
+        super.viewDidLoad()
+//        hides completed and deleted buttons if new todo
+        if todo == nil {
+            buttonStack.isHidden = true
         }
-        
-        
-        @IBAction func saveTask(_ sender: Any) {
-            if(checkTitle())
-            {
-                if todo == nil
-                {
-                    delegate?.saveTodo(title: todoTitleLabel!.text!, dueDate: deadlineLabel!.date)
-                }
-                else
-                {
-                    todo?.name = todoTitleLabel!.text!
-                    todo?.due_date = deadlineLabel!.date
-                    delegate?.updateTodo()
-                }
-                navigationController?.popViewController(animated: true)
-            }
+//        sets the field values if old todo opened
+        if let todoData = todo
+        {
+            todoTitleLabel.text = todoData.name
+            deadlineLabel.date = todoData.due_date!
         }
-        
-        @IBAction func markCompleted(_ sender: Any) {
-            
-            if(checkTitle()) {
+    }
+    
+    
+    @IBAction func saveTask(_ sender: Any) {
+        if(checkTitle())
+        {
+            if todo == nil
+            {
+                delegate?.saveTodo(title: todoTitleLabel!.text!, dueDate: deadlineLabel!.date)
+            }
+            else
+            {
                 todo?.name = todoTitleLabel!.text!
                 todo?.due_date = deadlineLabel!.date
-                delegate?.markTodoCompleted()
-                navigationController?.popViewController(animated: true)
+                delegate?.updateTodo()
             }
-            
+            navigationController?.popViewController(animated: true)
         }
+    }
+    
+    @IBAction func markCompleted(_ sender: Any) {
         
-        @IBAction func deleteTask(_ sender: Any) {
-            
-            delegate?.deleteTodoFromList()
+        if(checkTitle()) {
+            todo?.name = todoTitleLabel!.text!
+            todo?.due_date = deadlineLabel!.date
+            delegate?.markTodoCompleted()
             navigationController?.popViewController(animated: true)
         }
         
-        
-    //    method to check weather title is empty or not
-        func checkTitle() -> Bool {
-            if (todoTitleLabel.text?.isEmpty ?? true) {
-                let alert = UIAlertController(title: "Title can't be blank!", message: "", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                return false
-            }
-            else {
-                return true
-            }
-        }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    
+    @IBAction func deleteTask(_ sender: Any) {
+        
+        delegate?.deleteTodoFromList()
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+//    method to check weather title is empty or not
+    func checkTitle() -> Bool {
+        if (todoTitleLabel.text?.isEmpty ?? true) {
+            let alert = UIAlertController(title: "Title can't be blank!", message: "", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return false
+        }
+        else {
+            return true
+        }
+    }
 }
